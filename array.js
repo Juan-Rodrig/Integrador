90bccaebf8000410d3cd1b928b3b1bfd6ba6fee9
Aldana <aldananunez663@gmail.com>
2023-11-16 21:12:19 -0300

    Modiqué el archivo array.js

--- array.js
+++ array.js
@@ -1,6 +1,9 @@
 const numeros = [1,2,3]
+const nums = [4,5,6]
 const [pos0,,pos2] = numeros
-console.log(pos0,pos2);
+const numerosTodos = [...numeros, ...nums]
 
+console.log(pos0,pos2);
 console.log(pos0 + pos2)
+console.log(numerosTodos)
 
