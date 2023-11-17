aa7929a849bd84e59578834557f557cd1a0f8bba
Juan-Rodrig <rodriguezvargas.028@gmail.com>
2023-11-16 22:08:01 -0300

    Se modifico

--- array.js
+++ array.js
@@ -12,7 +12,7 @@ const numeros2 = [4,5,6]
 const [ubicacion0,,ubicacion2] = numeros2
 console.log(ubicacion0,ubicacion2)
 
-let mensaje = "Acordarse de exportar el archivo"
+let mensaje = "Exportar el archivo"
 console.log(mensaje)
 
-console.log("Tiene que dar error")
+console.log("error")
